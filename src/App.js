12715cddb79a668878f05f0e2c096d0eb1ea9a19
danny <danielniedzwiedzki.1@gmail.com>
2021-04-09 16:04:27 -0400

    simple react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      //includes checks if the string value passed inside includes is inside the monster
+    );
+
+    return (
+      <div className='App'>
+        <h1>Monsters Rolodex</h1>
+
+        <SearchBox
+          placeholder = 'search'
+          handleChange= {this.onSearchChange }
+        />
+
+
+        <CardList monsters={filteredMonsters} />
+       {/*  //new filtered list gets passed ti the Cardlist */}
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
